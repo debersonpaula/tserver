6365fa03ca7dfaa3110d5ed48573003ad2a0dbd5
Deberson de Paula <31399556+debersonpaula@users.noreply.github.com>
2017-09-30 12:18:54 -0300

    added react

--- src/client.ts
+++ src/client.ts
@@ -1,1 +1,8 @@
-document.write('Still Working!!!');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import ReactApp from './react/app';
+
+ReactDOM.render(
+    React.createElement(ReactApp),
+    document.getElementById('app')
+);
---
+++ src/react/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class ReactApp extends React.Component{
+    constructor(){
+        super();
+    }
+    render():JSX.Element|null|false{
+        return (
+            <p>Generated from React App</p>
+        );
+    }
+}
+
+export default ReactApp;
